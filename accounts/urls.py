ae8d00557ae76418cd99ef5ae884cfd65d1c2ddf
ne-cookdev <pvisnakov26@gmail.com>
2025-04-21 22:46:39 +0300

    Version 0.2.2-alpha. Added getting user info by username.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_list, name='user_list')
+    path('', views.user_list, name='user_list'),
+    path('<str:username>', views.user_by_username, name='user_list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -10,3 +10,13 @@ def user_list(request, ):
     users = User.objects.all().order_by('username')
     serializer = UserSerializer(instance=users, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def user_by_username(request, username, ):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({'message': 'User doesn`t exist.'})
+    serializer = UserSerializer(instance=user)
+    return Response(serializer.data)
