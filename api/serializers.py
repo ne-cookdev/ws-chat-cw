fcada95041efbdbfc2e1ba4a48b860f4ec9bffd6
ne-cookdev <pvisnakov26@gmail.com>
2025-05-06 21:22:56 +0300

    Version 0.3.0-beta. Added id in Conversation serializer.

--- api/serializers.py
+++ api/serializers.py
@@ -30,4 +30,4 @@ class ConversationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Conversation
-        fields = ['initiator', 'receiver', 'message_set']
+        fields = ['id', 'initiator', 'receiver', 'message_set']
